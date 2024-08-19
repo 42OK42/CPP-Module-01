11b3e5dcff3931b5af3bce53480f28b79e4b5d0a
Oliver Krahl <okrahl@c4b4c3.42berlin.de>
2024-08-19 16:22:33 +0200

    done

--- ex03/src/main.cpp
+++ ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: okrahl <okrahl@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 17:03:57 by okrahl            #+#    #+#             */
-/*   Updated: 2024/08/19 12:38:25 by okrahl           ###   ########.fr       */
+/*   Updated: 2024/08/19 16:18:31 by okrahl           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,21 @@
 
 int main()
 {
-	{
-		Weapon club = Weapon("crude spiked club");
+	Weapon club = Weapon("golf club");
 
-		HumanA bob("Bob", club);
-		bob.attack();
-		club.setType("some other type of club");
-		bob.attack();
-	}
-	{
-		Weapon club = Weapon("crude spiked club");
+	HumanA bob("Bob", club);
+	bob.attack();
+	club.setType("spiked club");
+	bob.attack();
 
-		HumanB jim("Jim");
-		/* jim.attack(); */
-		jim.setWeapon(club);
-		jim.attack();
-		club.setType("some other type of club");
-		jim.attack();
-	}
+	Weapon gun = Weapon("pistole");
+
+	HumanB jim("Jim");
+	jim.attack();
+	jim.setWeapon(gun);
+	jim.attack();
+	gun.setType("machine club");
+	jim.attack();
+
 	return 0;
 }
